851ace81b3266f3e4afa2b797dfab1f99615c362
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-19 13:45:40 -0500

    Update popup.js
    Updated url

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const copyButton = document.getElementById("copyButton");
 
   copyButton.addEventListener("click", () => {
-    const urlToCopy = "https://example.com"; // Replace with the URL you want to copy
+    const urlToCopy = "https://app.qbo.intuit.com/app/reportv2?mem_rpt_id=16"; // Replace with the URL you want to copy
     
     // Send a message to the content script to navigate and copy content
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
